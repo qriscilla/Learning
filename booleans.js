703a45665085f0ea58ca8e37873ba8fbf71bfe5e
Priscilla <49591938+qriscilla@users.noreply.github.com>
2020-01-09 16:54:12 -0500

    Reviewed conditions

--- booleans.js
+++ booleans.js
@@ -30,3 +30,25 @@ Logical Operators // Used to determine the logic between variables or values
   
 Conditional (Ternary) Operator // assigns a value to a variable based on some condition
   variablename = (condition) ? value1:value2
+
+Conditional Statements
+
+  if (condition) {
+    // block of code to be executed if the condition is true
+  } else {
+    // block of code to be executed if the condition is false
+  }
+
+  if (condition 1) {
+    // block of code to be executed if condition 1 is true
+  } else if (condition 2) {
+    // block of code to be executed if condition 1 is false and condition 2 is true
+  } else {
+    // block of code to be executed if condition 1 is false and condition 2 is false
+  }
+
+  // Note that these words (if, else, switch) are lowercase!
+  if // useed to specify a block of code to be executed, if a specified condition is true
+  else // used to specify a block of code to be executed, if the same condition is false
+  else if // used to specify a new condition to test, if the first condition is false
+  switch // used to specify many alternative blocks of code to be executed
