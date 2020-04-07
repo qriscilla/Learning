6b3ba3017853e107b334ef7b7763999025813c31
Priscilla <49591938+qriscilla@users.noreply.github.com>
2020-04-07 15:54:06 -0400

    declaring and displaying variables, rules for naming

--- C#/variables.cs
+++ C#/variables.cs
@@ -1,24 +1,5 @@
 // Variables are containers for storing data values
 
-// Data Types
-
-  // Numbers
-    int       -123 123        // Integers from -2,147,483,648 to 2,147,483,647
-                              // No special ending
-    long      15000000000L    // Integers from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
-                              // End with "L"
-    float     5.75F           // Fractional / decimal numbers up to 6 to 7 decimal digits
-              35e3F           // End with "F" and add an "e" to indicate power of 10
-    double    19.99 19.99D    // Fractional / decimal numbers up to 15 decimal digits
-              12E4D           // Optionally end with "D" and add an "e" to indicate power of 10
-                              // Allows more digits after the decimal point, so more "precise" than float
-  // Characters
-    char      'a' 'B'         // single character in 'single quotes'
-    string    "Hello World!"  // text in "double quotes"
-      
-  // Booleans
-    bool      true  false     // commonly used for conditional testing
-
 // Declaring or Creating Variables
 
   type identifier = value;      // Specify the type of a variable (identifier) and assign it a value
