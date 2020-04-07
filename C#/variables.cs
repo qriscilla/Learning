5d7e75bc7ed4da2e59c064c49f24458e2a55db68
Priscilla <49591938+qriscilla@users.noreply.github.com>
2020-04-07 14:59:58 -0400

    data types, const

---
+++ C#/variables.cs
@@ -0,0 +1,43 @@
+// Variables are containers for storing data values
+
+// Types
+
+  int       123 -123        // stores whole number integers without decimals
+  double    19.99 -19.99    // stores floating point number with decimals
+  char      'a' 'B'         // stores single characters in 'single quotes'
+  string    "Hello World!"  // stores text in "double quotes"
+  bool      true  false     // basically same as JS boolean
+
+// Declaring or Creating Variables
+
+  type identifier = value;      // Specify the type of a variable (identifier) and assign it a value
+  string name = "John";        
+  int myNum = 15;
+  
+  init myNum;                   // You can declare a variable without assigning any value at the moment
+  myNum = 15;                   // and assign one later
+  
+  init myNum = 15;              // Assign a new value to an existing variable? It'll overwrite the previous value
+  myNum = 20;                   // myNum is 20 now
+  
+  const int myNum = 15;         // To make a variable unchangeable and read-only, declare it as a const
+  myNum = 20;                   // error
+                          
+  const init myNum;             // error; You can't declare a const variable without assigning any value at the moment
+  
+  init x = 5, y = 6, x = 50;    // Use comma to declare more than one variable of the same type
+
+/* Rules for variables (unique identifiers)
+  Can contain letters, digits, and underscore _
+  Must begin with a letter
+  lowerCamelCase
+  Are case-sensitive
+  Can't contain any whitespace
+  Can't be reserved words (e.g., int, double, string) */
+
+// Displaying Variables
+  
+  string firstName = "Priscilla";
+  string lastName = "Whang";
+  string fullName = firstName + lastName;
+  Console.WriteLine(fullName);                  // Use WriteLine() to print to the console window
