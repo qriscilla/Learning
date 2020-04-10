3a8622224514f28be91da44179b8c22d16d6da8b
Priscilla <49591938+qriscilla@users.noreply.github.com>
2020-04-09 22:18:14 -0400

    arithmetic, assignment, comparison, logical operators

---
+++ C#/operators.cs
@@ -0,0 +1,42 @@
+/* Arithmetic Operators
+    Operator      Name                Description
+    +             Addition            Adds
+    -             Subtraction         Subtracts
+    *             Multiplication      Multiplies
+    /             Division            Divides
+    %             Modulus             Returns division remainder
+    ++            Increment           Increases value by 1
+    --            Decrement           Decreases value by 1
+*/
+
+/* Assignment Operators
+    Operator      Example          Same As
+    =             x = 5            x = 5
+    +=            x += 3           x = x + 3
+    -=            x -= 3           x = x - 3
+    *=            x *= 3           x = x * 3
+    /=            x /= 3           x = x / 3
+    %=            x %= 3           x = x % 3
+    &=            x &= 3           x = x & 3
+    |=            x |= 3           x = x | 3
+    ^=            x ^= 3           x = x ^ 3
+    >>=           x >>= 3          x >> 3
+    <<=           x <<= 3          x << 3
+*/
+
+/* Comparison Operators
+    Operator    Name                        Example
+    ==          Equal to                    x == y
+    !=          Not equal                   x != y
+    >           Greater than                x > y
+    <           Less than                   x < y
+    >=          Greater than or equal to    x >= y
+    <=          Less than or equal to       x <= y
+*/
+
+/* Logical Operators
+    Operator    Name            Description               Example, int x = 5
+    &&          Logical and     True if both are true     x < 5 && x < 10     true
+    ||          Logical or      True if one is true       x < 5 || x < 4      true
+    !           Logical not     Reverses                  !(x < 5 && x < 10)  false
+*/
