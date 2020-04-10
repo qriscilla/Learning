5d7cd0890dfaca5d2c1dcd4a584c323e8db10523
Priscilla <49591938+qriscilla@users.noreply.github.com>
2020-04-10 10:45:06 -0400

    Max, Min, Sqrt, Abs, Round

---
+++ C#/math-methods.cs
@@ -0,0 +1,24 @@
+Math.Max(x,y)   //  Used to find the highest value of x and y
+                //  Example
+                    Math.Max(5, 10);     
+                    10
+                    
+Math.Min(x,y)   //  Used to find the lowest value of x and y
+                //  Example
+                    Math.Min(5, 10);
+                    5
+                    
+Math.Sqrt(x)    //  Returns the square root of x
+                //  Example
+                    Math.Sqrt(64);
+                    8
+                    
+Math.Abs(x)     //  Returns the absolute value of x
+                //  Remember, absolute value is always positive
+                //  Example
+                    Math.Abs(-4.7);
+                    4.7
+                    
+Math.Round()    //  Rounds a number to the nearest whole number
+                //  Example
+                    Math.Round(9.99);
