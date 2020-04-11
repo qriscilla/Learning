de96a464339c65ca9ca385d0176b5d590dafb226
Priscilla <49591938+qriscilla@users.noreply.github.com>
2020-04-10 22:40:45 -0400

    Basic booleans overview

---
+++ C#/booleans.cs
@@ -0,0 +1,11 @@
+Boolean Expressions   //  Is an expression that returns a Boolean value, true or false
+                      //  Is the basis for all C# comparisons and conditions
+
+                      //  Can use a comparison operator, such as > (greater than)
+                      //  Example 1
+                          Console.WriteLine(10 > 9);    //  True
+                          Console.WriteLine(10 < 9);    //  False
+                      //  Example 2
+                          int x = 10;
+                          Console.WriteLine(x == 10);   //  True
+                          Console.WriteLine(10 == 15);  //  False
